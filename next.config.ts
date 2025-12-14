6e93b0f9ed16bfbe13443efc8d7bd4d2dbbf95af
Gustavo da Hora <gustadahora68@gmail.com>
2025-12-14 03:53:29 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/tts-app",
   images: {
     unoptimized: true,
   },
